11ee85a332336d485d0da81a8ee189d38c34c749
Karel Mareš <mares@flowly.cz>
2021-03-17 23:44:49 +0100

    hehe

--- rio_tiler/utils.py
+++ rio_tiler/utils.py
@@ -555,7 +555,8 @@ def tile_exists(bounds, tile_z, tile_x, tile_y):
     """
     mintile = mercantile.tile(bounds[0], bounds[3], tile_z)
     maxtile = mercantile.tile(bounds[2], bounds[1], tile_z)
-
+    print(mintile)
+    print(maxtile)
     return (
         (tile_x <= maxtile.x + 1)
         and (tile_x >= mintile.x)
